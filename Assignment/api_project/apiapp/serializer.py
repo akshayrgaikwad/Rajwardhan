92937c32ecf0a14930d149a5037a83efbd3932fc
kamleshnizare <143099644+kamleshnizare@users.noreply.github.com>
2023-12-25 14:48:13 +0530

    Delete Assignment directory

--- Assignment/api_project/apiapp/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import EmployeeModel
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EmployeeModel
-        fields = '__all__'
-
